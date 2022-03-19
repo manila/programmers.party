f0291424da8179ff845b31d0e0e5ffb976f49442
Manuel Nila <3621543+manila@users.noreply.github.com>
2022-03-19 05:54:05 +0000

    added README.md and install script

---
+++ README.md
@@ -0,0 +1,12 @@
+# Programmers.Party
+
+## Install
+```sh
+curl -o install.sh https://raw.githubusercontent.com/manila/programmers.party/main/install.sh
+```
+```sh
+chmod +x install.sh
+```
+```sh
+./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+URL="https://raw.githubusercontent.com/manila/programmers.party/main/programmers.party.7"
+MANN="programmers.party.7"
+MANS="man7"
+MANB="$( manpath | cut -d: -f1 )"
+MAND="$MAND/$MAND"
+
+curl -o "/tmp/$MANN" $URL
+gzip "/tmp/$MANN"
+
+if [[ ! -e "$MAND" ]]; then
+        mkdir -p "$MAND"
+fi
+
+cp "/tmp/${MANN}.gz" $MAND
+mandb
